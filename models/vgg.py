705ce524a9d1feaf2d4578222414ea280c79aed2
vandit15 <jainvandit15@gmail.com>
2019-10-14 04:17:45 -0700

    adds vgg: all versions

---
+++ models/vgg.py
@@ -0,0 +1,137 @@
+import tensorflow as tf
+
+from tensorflow.keras import layers
+from tensorflow.keras import Model
+
+
+class VGG(Model):
+	def __init__(self, features, num_classes=1000):
+		super(VGG, self).__init__()
+		self.features = features
+		self.classifier = tf.keras.Sequential(
+			[layers.Dense(4096),
+			layers.ReLU(),
+			layers.Dropout(0.5),
+			layers.Dense(4096),
+			layers.ReLU(),
+			layers.Dropout(0.5),
+			layers.Dense(num_classes)
+			])
+		self.flatten = layers.Flatten()
+
+	def call(self, x):
+		print(x.shape)
+		x = self.features(x)
+		print(x.shape)
+		# x = self.avgpool(x)
+		print(x.shape)
+		x = self.flatten(x)
+		x = self.classifier(x)
+		return x
+
+def make_layers(cfg, batch_norm = False):
+	layer = []
+	in_channels = 3
+	for v in cfg:
+		if v == 'M':
+			layer += [layers.MaxPool2D(pool_size = (2,2), strides = 2,
+			 padding = 'valid')]
+		else:
+			conv2d = layers.Conv2D(v, kernel_size = (3,3), padding = 'same')
+			if batch_norm:
+				layer += [conv2d, layers.BatchNormalization(), layers.ReLU()]
+			else:
+				layer += [conv2d, layers.ReLU()]
+	return tf.keras.Sequential(layer)
+
+cfgs = {
+    'A': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'B': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'D': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'E': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+}
+
+
+
+def _vgg(arch, cfg, batch_norm, **kwargs):
+    model = VGG(make_layers(cfgs[cfg], batch_norm=batch_norm), **kwargs)
+    return model
+
+
+def vgg11(**kwargs):
+    r"""VGG 11-layer model (configuration "A") from
+    `"Very Deep Convolutional Networks For Large-Scale Image Recognition" <https://arxiv.org/pdf/1409.1556.pdf>`_
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+    """
+    return _vgg('vgg11', 'A', False, **kwargs)
+
+
+def vgg11_bn(**kwargs):
+    r"""VGG 11-layer model (configuration "A") with batch normalization
+    `"Very Deep Convolutional Networks For Large-Scale Image Recognition" <https://arxiv.org/pdf/1409.1556.pdf>`_
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+    """
+    return _vgg('vgg11_bn', 'A', True, **kwargs)
+
+
+def vgg13(**kwargs):
+    r"""VGG 13-layer model (configuration "B")
+    `"Very Deep Convolutional Networks For Large-Scale Image Recognition" <https://arxiv.org/pdf/1409.1556.pdf>`_
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+    """
+    return _vgg('vgg13', 'B', False,**kwargs)
+
+
+
+def vgg13_bn(**kwargs):
+    r"""VGG 13-layer model (configuration "B") with batch normalization
+    `"Very Deep Convolutional Networks For Large-Scale Image Recognition" <https://arxiv.org/pdf/1409.1556.pdf>`_
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+    """
+    return _vgg('vgg13_bn', 'B', True,**kwargs)
+
+
+
+def vgg16(**kwargs):
+    r"""VGG 16-layer model (configuration "D")
+    `"Very Deep Convolutional Networks For Large-Scale Image Recognition" <https://arxiv.org/pdf/1409.1556.pdf>`_
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+    """
+    return _vgg('vgg16', 'D', False, **kwargs)
+
+
+def vgg19( **kwargs):
+    r"""VGG 19-layer model (configuration "E")
+    `"Very Deep Convolutional Networks For Large-Scale Image Recognition" <https://arxiv.org/pdf/1409.1556.pdf>`_
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+    """
+    return _vgg('vgg19', 'E', False, **kwargs)
+
+
+
+def vgg19_bn(**kwargs):
+    r"""VGG 19-layer model (configuration 'E') with batch normalization
+    `"Very Deep Convolutional Networks For Large-Scale Image Recognition" <https://arxiv.org/pdf/1409.1556.pdf>`_
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+    """
+    return _vgg('vgg19_bn', 'E', True, **kwargs)
+
+
+if __name__ == "__main__":
+    model = vgg19()
+    out = model(tf.ones([10,224,224,3]))
+    print(out)
